e701e98fe422ae4815a3f8f39005440651aee983
dongzoolee <nant0313@gmail.com>
2021-01-29 16:21:15 +0900

    서브 도메인 사용O

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
-app.set('port', 7777)
+app.set('port', 7777);
+
 app.use('/', (req, res) => {
-    var ip = req.headers['forwarded'];
+    let ip = req.headers['forwarded'];
     if (ip.substr(0, 7) === "::ffff:") {
         ip = ip.substr(7)
     }
-    res.json({ ip: ip })
-})
+    res.json({ ip: ip });
+});
 const server = app.listen(app.get('port'), (req, res) => {
     console.log('server has started on ' + app.get('port'))
-})
+});
