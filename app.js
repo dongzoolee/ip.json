bd62e55be95558a92aeea1431f914e1a3fddb81f
dongzoolee <nant0313@gmail.com>
2021-01-29 16:34:30 +0900

    서브도메인 사용O

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 app.set('port', 7777);
 
 app.use('/', (req, res) => {
-    let ip = req.headers['forwarded'];
+    let ip = req.headers['forwarded'] || req.connection.remoteAddress;
     if (ip.substr(0, 7) === "::ffff:") {
         ip = ip.substr(7)
     }
